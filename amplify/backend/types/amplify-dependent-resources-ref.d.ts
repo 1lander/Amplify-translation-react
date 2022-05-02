256137f0a8e54d923aa179b3fa0783515365ac94
Lander <landervanlandeghem@hotmail.com>
2022-05-02 13:54:52 +0200

    add predictions

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "translationsreact56b74880": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "predictions": {
+        "identifyText018c17e4": {
+            "region": "string",
+            "format": "string"
+        }
+    }
+}
